49b88f6182d081aa96b9dbf1d1e7fd28ff882aa2
Dominik Feger <dk.feger@gmail.com>
2021-06-02 12:28:44 +0200

    Update invoice.js

--- sickShellScripts/invoicer/invoice.js
+++ sickShellScripts/invoicer/invoice.js
@@ -40,47 +40,6 @@ const generateInvoice = (invoice, filename, success, error) => {
   }
 }
 
-const invoice = {
-  logo: "https://hosting.cybertap.ch/images/logos/g0OExnSQHAk.gif",
-  from: "CyberTap -hosting\nFeger IT & Security\nBadenerstrasse 56\n8104 Weiningen ZH",
-  to: "Rangsarit Ehrsam",
-  currency: "chf",
-  number: "HOS-INV-106",
-  payment_terms: "Payment 30 days after invoice date",
-  items: [
-    {
-      name: "CyberTap 1GB Simple Hosting",
-      description:
-        "5 years subscription, 1GB webspace, 50GB/month traffic, up to 10 E-Mail accounts, up to 2 Domains & 4 Databases.",
-      quantity: 5,
-      unit_cost: 59,
-    },
-    {
-      name: "massage-geroldswil.ch domain",
-      quantity: 5,
-      description: "5 years subscription",
-      unit_cost: 9.97,
-    },
-    {
-      name: "massagegeroldswil.ch",
-      quantity: 5,
-      description: "5 years subscription",
-      unit_cost: 9.97,
-    },
-    {
-      name: "chalida-wellness.ch domain",
-      quantity: 5,
-      description: "5 years subscription",
-      unit_cost: 9.97,
-    },
-  ],
-  fields: {
-    tax: "%",
-  },
-  tax: 7.7,
-  notes: "Thanks for being an awesome customer!",
-  };
-
 generateInvoice(
   invoiceTemplate,
   "CyberTap-invoice-"+hash+".pdf",
